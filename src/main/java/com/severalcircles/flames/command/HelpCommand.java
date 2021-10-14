6293e277b7f46aa1e04750e79a32c3c7f01969ed
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-10-14 18:53:57 -0400

    Update src/main/java/com/severalcircles/flames/command/HelpCommand.java

--- src/main/java/com/severalcircles/flames/command/HelpCommand.java
+++ src/main/java/com/severalcircles/flames/command/HelpCommand.java
@@ -7,6 +7,6 @@ import net.dv8tion.jda.api.interactions.components.Button;
 public class HelpCommand implements FlamesCommand {
     @Override
     public void execute(SlashCommandEvent event, FlamesUser sender) {
-        event.reply(event.getUser().getName() + ", select a link below.").addActionRow(Button.link("https://flames.severalcircles.com", "Documentation"), Button.link("https://github.com/SeveralCircles/flames/issues/new/choose", "Suggest a Feature or Report a Bug"), Button.link("https://discord.gg/eDbwAYzN6B", "Join the Support Server")).complete();
+        event.reply(event.getUser().getName() + ", select a link below.").addActionRow(Button.link("https://flames.severalcircles.com", "Documentation"), Button.link("https://github.com/colorfulFlames/flames/issues/new/choose", "Suggest a Feature or Report a Bug"), Button.link("https://discord.gg/eDbwAYzN6B", "Join the Support Server")).complete();
     }
 }
