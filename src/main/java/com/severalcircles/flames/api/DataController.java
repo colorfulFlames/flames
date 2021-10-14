95ac37a0921987470a244c744c94b5295f32d5ea
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-10-14 11:43:19 -0400

    Revert "Remove API from this repository"
    This reverts commit a7776159bc33f1aaca92910c6ca8173de519493a.

---
+++ src/main/java/com/severalcircles/flames/api/DataController.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Several Circles.
+ */
+
+package com.severalcircles.flames.api;
+
+import com.severalcircles.flames.data.base.FlamesDataManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@SuppressWarnings("ALL")
+@RestController
+public class DataController {
+    @GetMapping("/user/{id}")
+    public ResponseEntity getUser(@PathVariable String id) {
+        try {
+            return new ResponseEntity(FlamesDataManager.readUser(id).createData(), HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity("<!DOCTYPE html><html><head><script type=\"text/javascript\">window.location=\"https://flamesapi.severalcircles.com/error/404\"</script></head></html>",HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/com/severalcircles/flames/api/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2021 Several Circles.
+ */
+/**
+ * Flames API
+ */
+package com.severalcircles.flames.api;
