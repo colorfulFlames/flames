606b4d8fff6e66bcfc8f4270e4afe63c66de035c
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-10-15 10:44:39 -0400

    Added percents to StatsButtonAction

--- src/main/java/com/severalcircles/flames/buttonaction/data/StatsButtonAction.java
+++ src/main/java/com/severalcircles/flames/buttonaction/data/StatsButtonAction.java
@@ -16,13 +16,14 @@ public class StatsButtonAction implements ButtonAction {
     @Override
     public void execute(ButtonClickEvent event, FlamesUser user) {
         UserStats stats = user.getStats();
+        int next = Math.max(GlobalData.averageScore * 10 * stats.getLevel() * 1000, (stats.getLevel() + 1000 - GlobalData.participants));
         MessageEmbed embed = new EmbedBuilder()
                 .setColor(new Color(153, 85,187))
                 .setAuthor("User Data: Stats", null, event.getUser().getAvatarUrl())
                 .setTitle(event.getUser().getName())
                 .setDescription("Level " + stats.getLevel())
                 .addField("EXP", stats.getExp()+ "", true)
-                .addField("Estimated To Next Level", (Math.max(GlobalData.averageScore * 10 * stats.getLevel() * 1000, (stats.getLevel() + 1000 - GlobalData.participants))) - stats.getExp() + "", true)
+                .addField("Estimated To Next Level", (next - stats.getExp() + " (" + Math.round((stats.getExp()/next) * 100) + "%)"), true)
                 .addField("Power", stats.getPOW() + "", true)
                 .addField("Resistance", stats.getRES() + "", true)
                 .addField("Luck", stats.getLUCK() + "", true)
