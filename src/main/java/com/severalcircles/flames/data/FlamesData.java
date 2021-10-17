76ee1268cc80bcd5b9e9d2738741118701a86a07
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-10-17 14:45:30 -0400

    Added guild data

--- src/main/java/com/severalcircles/flames/data/FlamesData.java
+++ src/main/java/com/severalcircles/flames/data/FlamesData.java
@@ -4,8 +4,11 @@
 
 package com.severalcircles.flames.data;
 
+import java.util.Properties;
+
 /**
  * Represents the mere concept of Flames and it's data. Doesn't do much but sit here menacingly.
  */
 public interface FlamesData {
+    Properties createData();
 }
