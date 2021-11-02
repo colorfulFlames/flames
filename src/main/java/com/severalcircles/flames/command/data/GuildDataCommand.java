993371a8de7ce394aa68b921313dc9ebd6c1bd1b
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2021-11-02 12:45:31 -0400

    Added GuildDataEmbed

--- src/main/java/com/severalcircles/flames/command/data/GuildDataCommand.java
+++ src/main/java/com/severalcircles/flames/command/data/GuildDataCommand.java
@@ -5,60 +5,30 @@
 package com.severalcircles.flames.command.data;
 
 import com.severalcircles.flames.command.FlamesCommand;
-import com.severalcircles.flames.data.base.ConsentException;
 import com.severalcircles.flames.data.base.FlamesDataManager;
 import com.severalcircles.flames.data.guild.FlamesGuild;
 import com.severalcircles.flames.data.guild.NewGuildException;
 import com.severalcircles.flames.data.user.FlamesUser;
-import com.severalcircles.flames.features.Emotion;
-import com.severalcircles.flames.features.StringUtils;
-import com.severalcircles.flames.features.rank.Ranking;
-import com.severalcircles.flames.system.Flames;
-import net.dv8tion.jda.api.EmbedBuilder;
+import com.severalcircles.flames.features.info.data.GuildDataEmbed;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 
-import java.awt.*;
 import java.io.IOException;
-import java.time.Instant;
 
 public class GuildDataCommand implements FlamesCommand {
 //    ResourceBundle resources = ResourceBundle.getBundle("commands/MyDataCommand", Locale.ENGLISH);
     @Override
     public void execute(SlashCommandEvent event, FlamesUser sender) {
         Guild guild = event.getGuild();
-        FlamesGuild gdata;
+        FlamesGuild gdata = null;
         try {
-            gdata = FlamesDataManager.readGuild(guild.getId());
+            gdata = FlamesDataManager.readGuild(event.getGuild().getId());
         } catch (IOException e) {
             e.printStackTrace();
-            return;
         } catch (NewGuildException e) {
-            event.reply("Something went wrong. Mind trying that again?");
-            return;
+            // TODO: Add error message
+            e.printStackTrace();
         }
-        FlamesUser owner;
-        try {
-            owner = FlamesDataManager.readUser(guild.getOwner().getUser());
-        } catch (IOException | ConsentException | NullPointerException e) {
-            owner = new FlamesUser();
-        }
-        float emotion = gdata.getEmotion();
-        String emotionString = Emotion.getEmotionString(emotion, sender.getConfig().getLocale());
-        MessageEmbed embed = new EmbedBuilder()
-                .setAuthor("Guild Data", null, event.getUser().getAvatarUrl())
-                .setTitle(guild.getName())
-                .setDescription("Using Flames since " + StringUtils.prettifyDate(gdata.getJoined()))
-                .setThumbnail(guild.getIconUrl())
-                .addField("Flames Score", StringUtils.formatScore(gdata.getFlamesScore()), true)
-                .addField("Rank", Ranking.getRank((int) Math.round(gdata.getFlamesScore() / guild.getMemberCount())).toString(), true)
-//                .addField("Owner", guild.getOwner().getEffectiveName() + " (" + Ranking.getRank(owner.getScore()).toString() + ")", true)
-                .addField("Feeling", emotionString, true)
-                .setTimestamp(Instant.now())
-                .setColor(Color.magenta.darker())
-                .setFooter(Flames.api.getSelfUser().getName(), Flames.api.getSelfUser().getAvatarUrl())
-                .build();
-        event.replyEmbeds(embed).complete();
+        event.replyEmbeds(new GuildDataEmbed(event.getUser(), sender, gdata, guild).get()).complete();
     }
 }
