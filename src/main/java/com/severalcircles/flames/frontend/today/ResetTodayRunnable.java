453d24b3bffd952f659df316471eaf4ef6bceef0
Aidan Veney <62819140+SeveralCircles@users.noreply.github.com>
2023-02-15 10:58:28 -0500

    Finish cleanup

--- src/main/java/com/severalcircles/flames/frontend/today/ResetTodayRunnable.java
+++ src/main/java/com/severalcircles/flames/frontend/today/ResetTodayRunnable.java
@@ -5,7 +5,7 @@
 package com.severalcircles.flames.frontend.today;
 
 import com.severalcircles.flames.external.analysis.Analysis;
-import com.severalcircles.flames.frontend.today.qotd.QOTD;
+//import com.severalcircles.flames.frontend.today.qotd.QOTD;
 
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
@@ -30,6 +30,6 @@ public class ResetTodayRunnable implements Runnable {
         System.out.println(Calendar.getInstance().toInstant().truncatedTo(ChronoUnit.DAYS).compareTo(thanksgiving.toInstant().truncatedTo(ChronoUnit.DAYS)));
         if (Calendar.getInstance().toInstant().truncatedTo(ChronoUnit.DAYS).compareTo(thanksgiving.toInstant().truncatedTo(ChronoUnit.DAYS)) == 0) Today.isThanksgiving = true;
         Today.thanksgivingThanks = new HashMap<>();
-        QOTD.nextQuestion();
+//        QOTD.nextQuestion();
     }
 }
